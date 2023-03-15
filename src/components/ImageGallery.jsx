85e68df7e7726e59d16f67a55dcedc9397bf3c1e
KharlanEd <eduardxarlan@gmail.com>
2023-03-15 23:24:39 +0200

    fix

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -9,6 +9,7 @@ import {getImage} from "./service/imgApi"
 export function ImageGallery ({onClick,imgName}) {
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
+  
   const [status,setStatus]= useState('init')
  
   // state = {
@@ -16,42 +17,73 @@ export function ImageGallery ({onClick,imgName}) {
   //   images: [],
   //   status: 'id',
   // };
-
-
+  useEffect(() => {
+    if (imgName) {
+      setPage(1)
+    }
+  },[imgName])
+ 
   
   useEffect(() => {
+    
     if (!imgName) {
       return
     }
-    
-    async function getData() {
-       try {  
-        const newData = await getImage(imgName);
-         setImages(newData);
-         setStatus('resolved') 
+     
+      
+       async function getData() {
+      try { 
+         setStatus('pending')
+        const newData = await getImage(imgName, page);
+        
+        setImages(newData);
+        
+        setStatus('resolved') 
+        if (page > 1) {
+          setImages(prevState => [...prevState, ...newData])
+
+        }
+        
       } catch {
         setStatus('error')
       }
-    }
-  getData()
-  },[imgName])
-  
-  useEffect(() => {
-    if (page > 1) {
-      async function getPage() {
-        const newPage = await getImage(imgName, page);
-        setImages(prevState => [...prevState, ...newPage])
       }
-      getPage()
+      
+      getData()
+     
     }
     
-  },[imgName,page])
-
+   
+  
+  ,[imgName, page])
+  
+  // useEffect(() => {
+  
     
+  //   if (page < 1) {
+      
+  //     async function getPage() {
+  //       const newPage = await getImage(imgName, page);
+        
+  //       setImages(prevState => [...prevState, ...newPage])
+        
+  //     }
+  //     getPage()
+     
+      
+      
+  //   }
+  //   return
+    
+  // },[imgName, page])
+
+   
        
-const  handleLoadMore = () => {
-    setPage(prevState => (prevState + 1) );
-  };
+  const handleLoadMore = () => {
+   
+    setPage( page + 1);
+  
+  }
 
   
 
@@ -81,7 +113,7 @@ const  handleLoadMore = () => {
             
     )
      }
-   
+  
     
   }
 
